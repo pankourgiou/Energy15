280c0053445bd4e9699d9be501300b1faf02792a
Wagaguya <60981745+pankourgiou@users.noreply.github.com>
2026-05-27 07:10:34 +0300

    Update and rename Fun2.py to energy.swift

---
+++ energy.swift
@@ -0,0 +1,11 @@
+//setting soad = 1 (soad is system of a down..)we have:
+import Foundation
+
+let bpm = 70.0
+let beatDuration = 60.0 / bpm // seconds per beat
+
+while true {
+    print("E = soad*mc**2")
+    fflush(stdout)
+    Thread.sleep(forTimeInterval: beatDuration)
+}
